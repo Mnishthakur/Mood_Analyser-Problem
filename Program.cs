191537be1226e15f3950508ed7f108122d11983c
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-25 18:34:27 +0530

    [Manish Thakur]: Added Program.cs

--- Program.cs
+++ Program.cs
@@ -3,18 +3,19 @@ using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace AnalyserTest
-{
-    class Program
+namespace AnalyserTest;
+class Program
 {
     static void Main(string[] args)
     {
         TestAnalyser test = new TestAnalyser();
         test.Setup();
         test.TestAnalyseMood_SadMessage_ReturnsSad();
+        test.TestAnalyseMood_AnyMessage_ReturnsHappy();
     }
 }
-}
+
+
 
 
 
